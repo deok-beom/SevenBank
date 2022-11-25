92098a70ddf6e1adbf9805269fc0d36777f3dc80
byeongdoohan <quden04@gmail.com>
2022-11-25 17:05:23 +0900

    bank 수정

--- src/bank/controller/Bank.java
+++ src/bank/controller/Bank.java
@@ -23,47 +23,31 @@ public class Bank {
     }
 
     public void deleteAccount(Customer customer) {
-<<<<<<< HEAD
         for(Account account : bankAccounts) {
             if(customer.getName().equals(account.getOwnerName())) {
-=======
         for (Account account : bankAccounts) {
             if (customer.getName().equals(account.getOwnerName())) {
->>>>>>> upstream/main
                 bankAccounts.remove(customer);
             }
         }
     }
 
-<<<<<<< HEAD
-    public void checkAccountByCustomerName(Customer customer) { //소유자 명으로 계좌 조회
-        for(Account account : bankAccounts) {
-            if(customer.getName().equals(account.getOwnerName())) {
+    public void checkAccountByCustomerName(Customer customer){ //소유자 명으로 계좌 조회
+        for (Account account : bankAccounts) {
+            if (customer.getName().equals(account.getOwnerName())) {
                 String bankName = account.getBankName();
                 String accountNum = account.getAccountNumber();
                 int balance = account.getBalance();
-                System.out.printf("은행 : %s, 계좌번호 : %s, 잔고 : %d원",bankName,accountNum,balance);
+                System.out.printf("은행 : %s, 계좌번호 : %s, 잔고 : %d원", bankName, accountNum, balance);
             }
-=======
+        }
+    }
+
     public void findEveryAccountFromCustomerId(Customer customer) { //모든 계좌 정보 조회 기능
         String customerId = customer.getCustomerId();
         List<Account> accounts = customer.getAccounts();
         for (Account account : accounts) {
             System.out.println(account);
->>>>>>> upstream/main
         }
-    }
-
-
-    public void registerAccount(Account ac1) {
-    }
-
-    public void findEveryAccountFromCustomerId(Customer customer){ //모든 계좌 목록 조회
-        List<Account> temp = customer.getAccounts();
-        for (Account account : temp) {
-            System.out.println(account);
-        }
-
     }
 }
-
